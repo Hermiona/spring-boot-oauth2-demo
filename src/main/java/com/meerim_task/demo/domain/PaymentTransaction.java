4e719681233d8b8fa1501e4f60a5b46a6e1451ec
Meerim <merim.emm@gmail.com>
2020-09-18 18:30:17 +0600

    modify domain classes

--- src/main/java/com/meerim_task/demo/domain/PaymentTransaction.java
+++ src/main/java/com/meerim_task/demo/domain/PaymentTransaction.java
@@ -1,4 +1,39 @@
 package com.meerim_task.demo.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "payment_transactions")
 public class PaymentTransaction {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_balance_id")
+    private UserBalance userBalance;
+
+    @ManyToOne
+    @JoinColumn(name = "service_provider_id")
+    private ServiceProvider serviceProvider;
+
+    @Column(name = "amount", nullable = false)
+    private Integer amount;
+
+    @Column(name="transaction_timestamp", nullable = false)
+    private LocalDateTime transactionTimestamp;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusType status;
+}
+
+enum StatusType{
+    PENDING,
+    CANCELED,
+    COMPLETED
 }
--- src/main/java/com/meerim_task/demo/domain/UserBalance.java
+++ src/main/java/com/meerim_task/demo/domain/UserBalance.java
@@ -1,4 +1,21 @@
 package com.meerim_task.demo.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "user_balances")
 public class UserBalance {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "balance", nullable = false)
+    private Integer balance;
 }
