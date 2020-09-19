b74dde26bcebd5fe468217d2a5e0e80e5a03af1f
Meerim <merim.emm@gmail.com>
2020-09-20 00:57:49 +0600

    modify CancelPaymentTransactionRequest, CompletePaymentTransactionRequest, CreatePaymentTransactionRequest

--- src/main/java/com/meerim_task/demo/domain/request/CompletePaymentTransactionRequest.java
+++ src/main/java/com/meerim_task/demo/domain/request/CompletePaymentTransactionRequest.java
@@ -3,15 +3,16 @@ package com.meerim_task.demo.domain.request;
 import com.meerim_task.demo.domain.ServiceProvider;
 import com.meerim_task.demo.domain.StatusType;
 import com.meerim_task.demo.domain.UserBalance;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
 import java.time.LocalDateTime;
 
-@Value
+@RequiredArgsConstructor
 public class CompletePaymentTransactionRequest {
-    private UserBalance userBalance;
-    private ServiceProvider serviceProvider;
-    private Integer amount;
-    private LocalDateTime transactionTimestamp;
+    private final UserBalance userBalance;
+    private final ServiceProvider serviceProvider;
+    private final Integer amount;
+    private final LocalDateTime transactionTimestamp;
     private StatusType statusType = StatusType.COMPLETED;
 }
