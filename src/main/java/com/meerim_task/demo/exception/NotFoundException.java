f433f95a5ef84bb9e6a54050e79da502dc624f1d
Meerim <merim.emm@gmail.com>
2020-09-21 14:38:07 +0600

    modify NotFoundException, add ConflictException

--- src/main/java/com/meerim_task/demo/exception/NotFoundException.java
+++ src/main/java/com/meerim_task/demo/exception/NotFoundException.java
@@ -1,8 +1,18 @@
 package com.meerim_task.demo.exception;
 
+import org.springframework.data.util.Pair;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class NotFoundException extends Exception {
-    public NotFoundException(Class objectType, String attributeType, Object attribute) {
-        super("Failed to search %1$s with %2$s: %3$s".formatted(objectType.getSimpleName(), attributeType, attribute.toString()));
+
+    public NotFoundException(Class objectType, Pair<String, Object>... typeValues) {
+        super("Failed to search %1$s with %2$s".formatted(objectType.getSimpleName(), buildMessage(typeValues)));
+    }
+
+    private static String buildMessage(Pair<String, Object>... typeValues){
+        return Arrays.stream(typeValues).map(item -> "%1$s: %2$s".formatted(item.getFirst(), item.getSecond())).collect(Collectors.joining(", "));
     }
 }
 
